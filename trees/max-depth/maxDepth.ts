326c9d77f62e61ba9e249882787b338fefffc285
Brandie Lucano <blucano.dev@gmail.com>
2024-02-28 12:32:07 -0800

    working on maxDepth function

--- trees/max-depth/maxDepth.ts
+++ trees/max-depth/maxDepth.ts
@@ -11,11 +11,22 @@ function maxDepth(node: TreeNodeNum | null): number {
   // we then look at root's children
   // see if they have children,
   // ... if so, see if the children have children ... et
+  if(node === null) return 0;
 
   // somehow we are keeping count as we go "deep" into the stack
+  // const childValues = [];
+  // let count = 1
+
+  for(let child of node!.children){
+    count += maxDepth(child)
+    // childValues.push(maxDepth(child)); //B //D || //B //E //F || //C
+  }
+
+  // return Math.max(childValues.length + 1);
+  return Math.max(count + 1);
 
 
-  return 42;
+  // return 42;
 }
 
 export { maxDepth };
